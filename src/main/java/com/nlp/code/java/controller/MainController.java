9a284445d743d1f2d87cf1076c7f9161b2c750f0
Administrator <Administrator@SC-201912181659>
2020-11-19 21:31:06 +0800

    spark

--- src/main/java/com/nlp/code/java/controller/MainController.java
+++ src/main/java/com/nlp/code/java/controller/MainController.java
@@ -10,9 +10,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.nlp.code.java.entity.MonthTotalEntity;
 import com.nlp.code.java.entity.ProductEntity;
 import com.nlp.code.java.entity.ReviewEntity;
 import com.nlp.code.java.service.ReviewService;
+import com.nlp.code.spark.service.MonthTotalListService;
 import com.nlp.code.spark.service.ProductListService;
 
 import lombok.extern.slf4j.Slf4j;
@@ -26,9 +28,11 @@ public class MainController {
     private ReviewService reviewService;
     @Autowired
     private ProductListService productListService;
+    @Autowired
+    private MonthTotalListService monthTotalListService;
 
     /**
-     * 상품 리스트
+     * 리뷰 -- 상품 리스트
      * @return
      */
     @RequestMapping(value = "/queryList")
@@ -69,5 +73,39 @@ public class MainController {
         ReviewEntity result = new ReviewEntity();
         return result;
     }
+    
+    /**
+     * 월간 집계
+     * @param entity
+     * @return
+     * @throws Exception 
+     */
+    @RequestMapping(value = "/queryMonthTotal")
+    public String queryMonthTotalList(@RequestBody MonthTotalEntity entity,Model model) throws Exception {
+    	log.info("queryMonthTotalList start===");
+    	List<MonthTotalEntity> list = monthTotalListService.getMonthTotalList(entity);
+        model.addAttribute("monthTotalList",list);
+    	log.info("queryMonthTotalList end===");
+    	return "month_report";
+    }
 
+    
+    /**
+     * 월간집계 -- 상품 리스트
+     * @return
+     */
+    @RequestMapping(value = "/monthReport")
+    public String queryProductLists(Model model){
+        log.info("queryProductLists start===");
+        //List<ProductEntity> list = productListService.getProductList();
+        List<ProductEntity> list = new ArrayList<>();
+        ProductEntity entity = new ProductEntity();
+        entity.setAsin("1");
+        entity.setTitle("Book");
+        entity.setPrice("99.8");
+        list.add(entity);
+        model.addAttribute("productList",list);
+        log.info("queryProductLists end===");
+        return "month_report";
+    }
 }
--- src/main/java/com/nlp/code/spark/service/MonthTotalListService.java
+++ src/main/java/com/nlp/code/spark/service/MonthTotalListService.java
@@ -1,11 +1,22 @@
 package com.nlp.code.spark.service;
 
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.hadoop.hbase.CellUtil;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.Connection;
+import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.client.ResultScanner;
+import org.apache.hadoop.hbase.client.Scan;
+import org.apache.hadoop.hbase.client.Table;
+import org.apache.hadoop.hbase.util.Bytes;
 import org.springframework.stereotype.Service;
 
 import com.nlp.code.java.entity.MonthTotalEntity;
+import com.nlp.code.spark.conf.HBaseConnectionBean;
 
 /**
  * hbase에서 월간 집계 정보 가져오기
@@ -15,9 +26,45 @@ import com.nlp.code.java.entity.MonthTotalEntity;
 @Service
 public class MonthTotalListService {
 
-	public List<MonthTotalEntity> getMonthTotalList(MonthTotalEntity entity){
+	/**
+	 * 월간 집계 데이터 가져오기
+	 * @param entity
+	 * @return
+	 * @throws IOException
+	 */
+	public List<MonthTotalEntity> getMonthTotalList(MonthTotalEntity entity) throws IOException{
 		
+		List<MonthTotalEntity> list = new ArrayList<MonthTotalEntity>();
 		
-		return null;
+		//hbase연결
+		Connection connection =  HBaseConnectionBean.getConnection();
+		//테이블 설정
+		Table table =connection.getTable(TableName.valueOf("product_count_month_rpt"));
+		//검색조건 추가
+		Scan scan = new Scan();
+		String startRow = entity.getMonth()+entity.getAsin()+"00001";
+		String stopRow = entity.getMonth()+entity.getAsin()+"99999";
+		scan.setStartRow(Bytes.toBytes(startRow));
+		scan.setStopRow(Bytes.toBytes(stopRow));
+		//테이터 가져오기
+		ResultScanner scanResult = table.getScanner(scan);   
+		//결과를 MonthTotalEntity 오브젝에 넣기
+		for (Result result : scanResult) {
+			MonthTotalEntity en = new MonthTotalEntity();
+      	  	en.setAsin(Bytes.toString(CellUtil.cloneValue(
+      	  			result.getColumnLatestCell("info".getBytes(),"asin".getBytes()))));
+      	  	en.setTitle(Bytes.toString(CellUtil.cloneValue(
+      	  			result.getColumnLatestCell("info".getBytes(),"title".getBytes()))));
+      	  	en.setBrand((Bytes.toString(CellUtil.cloneValue(
+      	  			result.getColumnLatestCell("info".getBytes(),"brand".getBytes())))));
+      	  	en.setCount(Bytes.toString(CellUtil.cloneValue(
+      	  			result.getColumnLatestCell("statistics".getBytes(),"count".getBytes()))));
+      	  	en.setTotal(Bytes.toString(CellUtil.cloneValue(
+      	  			result.getColumnLatestCell("statistics".getBytes(),"total".getBytes()))));
+      	  	en.setMonth(Bytes.toString(CellUtil.cloneValue(
+      	  			result.getColumnLatestCell("statistics".getBytes(),"month".getBytes()))));
+      	  	list.add(en);
+		}
+		return list;
 	}
 }
